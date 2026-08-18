6cb44123a3a69aa09b128e9c1c3d0e837e947b9d
lunar72016 <lunar72016@gmail.com>
2026-08-18 09:54:24 +0800

    Delete client/src/lib/avatarPersistence.ts

--- client/src/lib/avatarPersistence.ts
+++
@@ -1,9 +0,0 @@
-export function awaitRequiredPersistence<T>(promise: Promise<T>, timeoutMs: number, reason: string) {
-  return new Promise<T>((resolve, reject) => {
-    const timeoutId = globalThis.setTimeout(() => reject(new Error(reason)), timeoutMs);
-    promise.then(
-      (value) => { globalThis.clearTimeout(timeoutId); resolve(value); },
-      (error) => { globalThis.clearTimeout(timeoutId); reject(error); },
-    );
-  });
-}
