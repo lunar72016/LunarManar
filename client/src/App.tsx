0196a917a1a45ec550ed4bff63872eb63786541d
lunar72016 <lunar72016@gmail.com>
2026-08-22 02:01:29 +0800

    v0.6

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,15 +2,18 @@ import ErrorBoundary from "./components/ErrorBoundary";
 import { FirebaseAuthProvider } from "./contexts/FirebaseAuthContext";
 import { ThemeProvider } from "./contexts/ThemeContext";
 import Home from "./pages/Home";
+import ClientPortalPage from "./pages/ClientPortalPage";
 import ProgressPlaceholder from "./pages/ProgressPlaceholder";
 
 function App() {
+  const hashRoute = window.location.hash.replace(/^#/, "");
+  const isClientRoute = hashRoute.startsWith("/client") || window.location.pathname.startsWith("/client");
   const isShareRoute = window.location.pathname.startsWith("/progress/");
   return (
     <ErrorBoundary>
       <ThemeProvider defaultTheme="light">
         <FirebaseAuthProvider>
-          {isShareRoute ? <ProgressPlaceholder /> : <Home />}
+          {isClientRoute ? <ClientPortalPage /> : isShareRoute ? <ProgressPlaceholder /> : <Home />}
         </FirebaseAuthProvider>
       </ThemeProvider>
     </ErrorBoundary>
